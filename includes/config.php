df44c93a5058cfffb26ccfead2e71b49259d2222
Mark <suguitanmark123@gmail.com>
2025-11-26 22:46:48 +0800

    first commit

---
+++ includes/config.php
@@ -0,0 +1,33 @@
+<?php
+    class Database{
+        private $host = 'localhost';
+        private $user = 'root';
+        private $pass = '';
+        private $dbname = 'sarias';
+        private $conn;
+
+        public function __construct(){
+            $this->connect();
+        }
+
+        private function connect(){
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+
+            // Check connection
+            if($this->conn->connect_error){
+                // Handle connection error
+                die("Connection failed:" . $this->conn->connect_error);
+            }
+            $this->conn->set_charset('utf8');
+        }
+
+        public function getConnection(){
+            return $this->conn;
+        }
+    }
+
+    //sample usage
+    $db = new Database();
+    $con = $db->getConnection();
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header class="bg-green-600 text-white p-4 flex items-center">
+    <h3 class="text-xl font-bold">Student Accounting and Registration </h3>
+</header>
